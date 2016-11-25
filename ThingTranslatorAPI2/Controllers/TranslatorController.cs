37b8fb54c6d6903b1acf77822ca6dc1545f19eef
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-11-25 02:04:47 +0100

    OK!

--- ThingTranslatorAPI2/Controllers/TranslatorController.cs
+++ ThingTranslatorAPI2/Controllers/TranslatorController.cs
@@ -9,16 +9,16 @@ using System.Web.Http;
 using System.Web.Http.Results;
 using Google.Apis.Services;
 using Google.Apis.Translate.v2;
+using Google.Apis.Translate.v2.Data;
 using Google.Apis.Vision.v1.Data;
+using GoogleApi;
+using GoogleApi.Entities.Translate.Translate.Request;
 using TranslationsResource = Google.Apis.Translate.v2.Data.TranslationsResource;
 
-namespace ThingTranslatorAPI2.Controllers
-{
+namespace ThingTranslatorAPI2.Controllers {
   [RoutePrefix("api")]
-  public class TranslatorController : ApiController
-    {
+  public class TranslatorController : ApiController {
     String apiKey = "AIzaSyCUD75r6fNhZE5Xa8TNJaAeAXrSWzg-BiM";
-    static String translated;
 
     public class MyMultipartFormDataStreamProvider : MultipartFormDataStreamProvider {
       public MyMultipartFormDataStreamProvider(string path)
@@ -26,7 +26,7 @@ namespace ThingTranslatorAPI2.Controllers
       }
     }
 
-    [Route("files")]
+    [Route("upload")]
     [HttpPost]
     public async Task<JsonResult<List<object>>> Upload() {
       if (!Request.Content.IsMimeMultipartContent())
@@ -35,58 +35,35 @@ namespace ThingTranslatorAPI2.Controllers
       IList<AnnotateImageResponse> result;
       var provider = new MultipartMemoryStreamProvider();
       await Request.Content.ReadAsMultipartAsync(provider);
-      foreach (var file in provider.Contents) {
-        if (file.Headers.ContentLength != 0) {
-          var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
-        }
-        var buffer = await file.ReadAsByteArrayAsync();
 
-        //Do whatever you want with filename and its binaray data.
-        result = LabelDetectior.GetLabels(buffer);
-        var bestGuess = result[0].LabelAnnotations.FirstOrDefault().Description;
-
-        new TranslatorController().Translate(bestGuess).Wait();
-
-        /*
-        var labels = new List<object>();
-        foreach (var lbl in result[0].LabelAnnotations)
-        {
-          labels.Add(new { Label = lbl.Description, Score = lbl.Score });
-        }
-        
-        var ret = Json(labels);
-        return ret;
-        */
+      var file = provider.Contents[0];
+      if (file.Headers.ContentLength != 0) {
+        var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
       }
+      var buffer = await file.ReadAsByteArrayAsync();
+      //Do whatever you want with filename and its binaray data.
+      result = LabelDetectior.GetLabels(buffer);
+      String bestGuess = result[0].LabelAnnotations.FirstOrDefault()?.Description;
+       String translated = TranslateText(bestGuess, "en", "hr");
 
-      var res = new List<object>();
-      res.Add(new { Translation = translated });
-      var x = Json(res);
-      return x;
-      //return Ok();
-    }
-
-    public async Task Translate(String srcText) {
-      var service = new TranslateService(new BaseClientService.Initializer() {
-        ApiKey = "AIzaSyCUD75r6fNhZE5Xa8TNJaAeAXrSWzg-BiM",
-        ApplicationName = "Translate API Sample"
-      });
-
-      // Execute the first translation request.
-      var response = await service.Translations.List(srcText, "bs").ExecuteAsync();
-      var translations = new List<string>();
-
-      foreach (TranslationsResource translation in response.Translations) {
-        translations.Add(translation.TranslatedText);
-        Console.WriteLine("translating " + srcText + ": " + translation.TranslatedText);
-      }
-      translated = translations[0];
+      var res = new List<object> { new { Translation = translated } };
+      return Json(res);
     }
 
     [Route("translate")]
     public IHttpActionResult GetTranslation() {
+      return Ok(TranslateText("Hello", "en", "hr"));
+    }
 
-      return Ok("translated");
+    private String TranslateText(String text, String source, String target) {
+      var _request = new TranslateRequest {
+        Source = source,
+        Target = target,
+        Qs = new[] { text },
+        Key = apiKey
+      };
+      var _result = GoogleTranslate.Translate.Query(_request);
+      return _result.Data.Translations.First().TranslatedText;
     }
 
     private StreamContent StreamConversion() {
