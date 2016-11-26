ead2497852b8dc7e6839901d7ed51fc83527b8d0
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-11-26 22:41:53 +0100

    GOOGLE_APPLICATION_CREDENTIALS

--- ThingTranslatorAPI2/LabelDetectior.cs
+++ ThingTranslatorAPI2/LabelDetectior.cs
@@ -5,13 +5,19 @@ using Google.Apis.Vision.v1.Data;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using Google.Apis.Discovery;
 
 namespace ThingTranslatorAPI2 {
 
   public class LabelDetectior {
+    
+
     /// <returns>an authorized Cloud Vision client.</returns>
-    public static VisionService CreateAuthorizedClient() {
+    public static VisionService CreateAuthorizedClient()
+    {
+
+      createEnvVar();
       try {
         GoogleCredential credential = GoogleCredential.GetApplicationDefaultAsync().Result;
         // Inject the Cloud Vision scopes
@@ -29,6 +35,20 @@ namespace ThingTranslatorAPI2 {
         Trace.TraceError("CreateAuthorizedClient: " + ex.StackTrace);
       }
       return null;
+    }
+
+    private static void createEnvVar()
+    {
+      var path = System.Web.Hosting.HostingEnvironment.MapPath("~/") + "VisionAPI-0a3feb1f1da5.json";
+      var exist = System.IO.File.Exists(  path);
+      if (exist)
+      {Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+        Trace.TraceError("createEnvVar: created " + path);
+      }
+      else
+      {
+        Trace.TraceError("createEnvVar: missing " + path  );
+      }
     }
 
     public static IList<AnnotateImageResponse> GetLabels(  string imagePath) {
