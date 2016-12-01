2cdf8558438d32ff7de2e8c6258b4a43c2f5d3ac
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-12-02 00:43:16 +0100

    createEnvVar

--- ThingTranslatorAPI2/Controllers/TranslatorController.cs
+++ ThingTranslatorAPI2/Controllers/TranslatorController.cs
@@ -42,7 +42,6 @@ namespace ThingTranslatorAPI2.Controllers {
       if (!Request.Content.IsMimeMultipartContent())
         throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
 
-      createEnvVar();
       String bestGuess, translated, langCode = string.Empty;
       IList<AnnotateImageResponse> result;
       var res = new Response();
@@ -119,27 +118,6 @@ namespace ThingTranslatorAPI2.Controllers {
       return (Environment.GetEnvironmentVariable("apiKey"));  
     }
 
-    private static void createEnvVar() {
-      /*Use your own VisionAPI key here
-       * To create new key go to : https://cloud.google.com/vision/docs/quickstart
-       */
-       var VisionApiKey = (Environment.GetEnvironmentVariable("VisionApiKey"));
-      var path = System.Web.Hosting.HostingEnvironment.MapPath("~/") + "VisionAPI-0a3feb1f1da5.json";
-      var exist = System.IO.File.Exists(path);
-      if (!exist)
-      {
-      File.WriteAllText(VisionApiKey, path);
-      }
-      Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
 
-      //if (exist) {
-      //  if (Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS") == null) {
-      //    Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
-      //    Trace.TraceError("createEnvVar: created " + path);
-      //  }
-      //} else {
-      //  Trace.TraceError("createEnvVar: missing " + path);
-      //}
-    }
   }
 }
--- ThingTranslatorAPI2/Global.asax.cs
+++ ThingTranslatorAPI2/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -12,6 +13,29 @@ namespace ThingTranslatorAPI2
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
+            createEnvVar();
     }
+
+    private static void createEnvVar() {
+      /*Use your own VisionAPI key here
+       * To create new key go to : https://cloud.google.com/vision/docs/quickstart
+       */
+      var VisionApiKey = (Environment.GetEnvironmentVariable("VisionApiKey"));
+      var path = System.Web.Hosting.HostingEnvironment.MapPath("~/") + "VisionAPI-0a3feb1f1da5.json";
+      var exist = System.IO.File.Exists(path);
+      if (!exist) {
+        File.WriteAllText(VisionApiKey, path);
+      }
+      Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+      //if (exist) {
+      //  if (Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS") == null) {
+      //    Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+      //    Trace.TraceError("createEnvVar: created " + path);
+      //  }
+      //} else {
+      //  Trace.TraceError("createEnvVar: missing " + path);
+      //}
+    }
+  }
 }
