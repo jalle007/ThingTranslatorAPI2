9cde4b2c0771dc8ac5659a1bdcb0b84da01ad0c4
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-12-02 01:07:27 +0100

    createEnvVar

--- ThingTranslatorAPI2/Global.asax.cs
+++ ThingTranslatorAPI2/Global.asax.cs
@@ -1,32 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
 
-namespace ThingTranslatorAPI2
-{
-    public class WebApiApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            createEnvVar();
+namespace ThingTranslatorAPI2 {
+  public class WebApiApplication : System.Web.HttpApplication {
+    protected void Application_Start() {
+      GlobalConfiguration.Configure(WebApiConfig.Register);
+      createEnvVar();
     }
 
+    /*Use your own VisionAPI key here
+   * To create new key go to : https://cloud.google.com/vision/docs/quickstart
+   */
     private static void createEnvVar() {
-      /*Use your own VisionAPI key here
-       * To create new key go to : https://cloud.google.com/vision/docs/quickstart
-       */
-      var VisionApiKey = (Environment.GetEnvironmentVariable("VisionApiKey"));
-      var path = System.Web.Hosting.HostingEnvironment.MapPath("~/") + "VisionAPI-0a3feb1f1da5.json";
-      var exist = System.IO.File.Exists(path);
-      if (!exist) {
-        File.WriteAllText(VisionApiKey, path);
+      var GAC = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+      if (GAC == null) {
+        var VisionApiKey = Environment.GetEnvironmentVariable("VisionApiKey");
+        if (VisionApiKey != null) {
+          var path = System.Web.Hosting.HostingEnvironment.MapPath("~/") + "VisionAPI-0a3feb1f1da5.json";
+
+          Trace.TraceError("path: " + path);
+
+          File.WriteAllText(VisionApiKey, path);
+          Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+        }
       }
-      Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
 
       //if (exist) {
       //  if (Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS") == null) {
