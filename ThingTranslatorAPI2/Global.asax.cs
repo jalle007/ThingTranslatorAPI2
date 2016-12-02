2c9561bcd453cd532f193c9d505dacfefe8c5f5e
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-12-02 03:37:34 +0100

    asdasdsd

--- ThingTranslatorAPI2/Global.asax.cs
+++ ThingTranslatorAPI2/Global.asax.cs
@@ -11,7 +11,7 @@ namespace ThingTranslatorAPI2 {
 
     protected void Application_Start() {
       GlobalConfiguration.Configure(WebApiConfig.Register);
-      apiKey =  (Environment.GetEnvironmentVariable("apiKey"));
+      apiKey = "asdasdsd";// Environment.GetEnvironmentVariable("apiKey");
       Application["apiKey"] = apiKey;
 
       createEnvVar();
